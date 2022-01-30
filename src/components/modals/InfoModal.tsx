b72251297e579280b7562cf8e7aac52280577334
wunnle <me@wunnle.com>
2022-01-30 19:57:49 +0300

    updated info modal

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,38 +10,38 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the WORDLE in 6 tries. After each guess, the color of the tiles
+        Guess the word in 6 tries. After each guess, the color of the tiles
         will change to show how close your guess was to the word.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="W" status="correct" />
-        <Cell value="E" />
-        <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
-        <Cell value="L" status="present" />
+        <Cell value="G" status="correct" />
         <Cell value="O" />
-        <Cell value="T" />
+        <Cell value="N" />
+        <Cell value="Z" />
+        <Cell value="O" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        The letter G is in the word and in the correct spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
-        <Cell value="A" />
-        <Cell value="G" />
-        <Cell value="U" status="absent" />
         <Cell value="E" />
+        <Cell value="K" />
+        <Cell value="M" status="present" />
+        <Cell value="E" />
+        <Cell value="K" />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        The letter M is in the word but in the wrong spot.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="T" />
+        <Cell value="A" />
+        <Cell value="V" />
+        <Cell value="U" status="absent" />
+        <Cell value="K" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         The letter U is not in the word in any spot.
